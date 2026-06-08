83e35751b070146e72685cebe154ec552da04185
mdrakibhasan12112 <mdrakibhasan12112@gmail.com>
2026-06-08 20:15:36 +0600

    create server and connect to forntend

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+
+
+const uri = process.env.MONGO_DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+   await client.connect();
+   
+  const database = client.db('hire-loop');
+  const jobCollection = database.collection('jobs');
+
+   app.post('/jobs', async (res, req) => {
+    const job = req.body;
+    const result = await jobCollection.insertOne(job)
+    res.send(result)
+   })
+   
+
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!',
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
